f6edf5fff928567652c46f4ebeaafe64d0a19169
RealKarthiksai <a.karthik0912@gmail.com>
2026-07-30 23:06:02 +0530

    Updates

--- assets/PermissionGate-CL-SbPmH.js
+++
@@ -1,1 +0,0 @@
-import{Gt as e}from"./vendor-react-Bp1dDGwy.js";import{a as t}from"./index-CZDT-0PJ.js";var n=e();function r({children:e,required:r,hasAny:i,hasAll:a,fallback:o=null}){let{can:s,hasAny:c,hasAll:l}=t(),u=!0;return r&&!s(r)&&(u=!1),i&&!c(i)&&(u=!1),a&&!l(a)&&(u=!1),u?(0,n.jsx)(n.Fragment,{children:e}):(0,n.jsx)(n.Fragment,{children:o})}export{r as t};
